c192612c2e1b4f8258937200edb5c565b1d4047c
cpratham3 <cpratham3@gmail.com>
2023-05-17 18:59:39 -0400

    ADDED toString methods

--- src/chapter_10PracticeSet.java
+++ src/chapter_10PracticeSet.java
@@ -8,7 +8,10 @@ class Circle{
        double a = Math.PI * this.radius * this.radius;
        return a;
     }
+    public String toString(){
 
+        return "Circle has radius = "+ this.radius + " and area = "+ Math.PI * this.radius * this.radius;
+    }
 }
 
 class Cylinder extends Circle{
@@ -19,13 +22,23 @@ class Cylinder extends Circle{
     }
     Cylinder(int r,int h) {
         super(r);
+        this.height = h;
+
+    }
+    public String toString(){
+
+        return "Cylinder has radius = "+ this.radius + " and height = "+ this.height + " with volume = " + Math.PI*this.radius*this.radius*height;
     }
 }
 
 
 public class chapter_10PracticeSet {
     public static void main(String[] args) {
+        Circle MyCircle = new Circle(12);
+        System.out.println(MyCircle);
+        System.out.println("__________________________");
+        Cylinder MyCylinder = new Cylinder(12,30);
+        System.out.println(MyCylinder);
 
-        System.out.println("Hello world!");
     }
 }
